e96a30dd7330386bf1fe23d16ff0eeeb2d2d013c
napolr1 <92889022+napolr1@users.noreply.github.com>
2022-06-17 17:24:19 -0400

    demo base

--- lib/inferno_template/patient_group.rb
+++ lib/inferno_template/patient_group.rb
@@ -11,34 +11,16 @@ module InfernoTemplate
     description 'Verify that the server makes Patient resources available'
     id :patient_group
 
-    test do
-      title 'Server returns requested Patient resource from the Patient read interaction'
-      description %(
-        Verify that Patient resources can be read from the server.
-      )
-
-      input :patient_id
-      # Named requests can be used by other tests
-     makes_request :patient
-
-      run do
-        fhir_read(:patient, patient_id, name: :patient)
-
-        assert_response_status(200)
-        assert_resource_type(:patient) 
-        assert resource.id == patient_id,
-               "Requested resource with id #{patient_id}, received resource with id #{resource.id}"
-      end
-    end
-
-    
+        
     test do
       title 'Patient match is valid'
       description %(
         Verify that the Patient  $match resource returned from the server is a valid FHIR resource.
       )
-      #input :search_json
-      #input :patient_id
+      
+      input :search_json ,
+        type: 'textarea'
+      
       output :response_json
       # Named requests can be used by other tests
       makes_request :match
@@ -50,158 +32,134 @@ module InfernoTemplate
       fhir_client  do
         url :url
       end
-      #fhir_client :with_custom_header do
-      #  url :url
-      #  headers { 'Content-Type': 'application/fhir+json' }
-     #end
+ 
       run do
-        #body = JSON.dump(search_json)
-          body = {
-            "resourceType": "Parameters",
-            "id": "example",
-            "parameter": [
-              {
-                "name": "resource",
-                "resource": {
-                  "resourceType": "Patient",
-                  "id": "1244780",
-                  "meta": {
-                    "versionId": "1",
-                    "lastUpdated": "2020-06-22T07:37:44.914+00:00",
-                    "source": "#Hep7oN0aHNPLwGdq",
-                    "tag": [
-                      {
-                        "system": "http://terminology.hl7.org/CodeSystem/v3-ObservationValue",
-                        "code": "SUBSETTED",
-                        "display": "Resource encoded in summary mode"
-                      }
-                    ]
-                  },
-                  "identifier": [
-                    {
-                      "type": {
-                        "coding": [
-                          {
-                            "system": "http://terminology.hl7.org/CodeSystem/v2-0203",
-                            "code": "DL",
-                            "display": "Drivers License"
-                          }
-                        ],
-                        "text": "Drivers License"
-                      },
-                      "value": "Q147604567"
+        body = JSON[search_json] 
+        body ={
+          "resourceType": "Parameters",
+          "id": "example",
+          "parameter": [
+            {
+              "name": "resource",
+              "resource": {
+                "resourceType": "Patient",
+                "id": "1244780",
+                "identifier": [
+                  {
+                    "type": {
+                      "coding": [
+                        {
+                          "system": "http://terminology.hl7.org/CodeSystem/v2-0203",
+                          "code": "DL",
+                          "display": "Drivers License"
+                        }
+                      ],
+                      "text": "Drivers License"
                     },
-                    {
-                      "type": {
-                        "coding": [
-                          {
-                            "system": "http://terminology.hl7.org/CodeSystem/v2-0203",
-                            "code": "NIIP",
-                            "display": "National Insurance Payor Identifier"
-                          }
-                        ],
-                        "text": "National Insurance Payor Identifier"
-                      },
-                      "value": "9800010077"
-                    }
-                  ],
-                  "name": [
-                    {
-                      "family": "Queentin",
-                      "given": [
-                        "Vladimir"
-                      ]
-                    }
-                  ],
-                  "maritalStatus": {
-                    "coding": [
-                      {
-                        "system": "http://terminology.hl7.org/CodeSystem/v3-MaritalStatus",
-                        "code": "M"
-                      }
+                    "value": "Q147604567"
+                  }
+                ],
+                "name": [
+                  {
+                    "family": "Queentin",
+                    "given": [
+                      "Vladimir"
                     ]
-                  },
-                  "telecom": [
+                  }
+                ],
+                "maritalStatus": {
+                  "coding": [
                     {
-                      "system": "phone",
-                      "value": "344-845-5689",
-                      "use": "mobile"
+                      "system": "http://terminology.hl7.org/CodeSystem/v3-MaritalStatus",
+                      "code": "M"
                     }
-                  ],
-                  "address": [
-                    {
-                      "type": "physical",
-                      "line": [
-                        "321 South Maple Street"
-                      ],
-                      "city": "Scranton",
-                      "state": "PA",
-                      "postalCode": "18503",
-                      "use": "home"
-                    }
-                  ],
-                  "contact": [
-                    {
-                      "relationship": [
-                        {
-                          "coding": [
-                            {
-                              "system": "http://hl7.org/fhir/v2/0131",
-                              "code": "C",
-                              "display": "Emergency Contact"
-                            }
-                          ]
-                        }
-                      ],
-                      "telecom": [
-                        {
-                          "system": "phone",
-                          "value": "726-555-1094",
-                          "use": "home"
-                        }
-                      ]
-                    }
-                  ],
-                  "gender": "male",
-                  "birthDate": "1956-12-01"
-                }
-              },
-              {
-                "name": "count",
-                "valueInteger": "3"
-              },
-              {
-                "name": "onlyCertainMatches",
-                "valueBoolean": "false"
+                  ]
+                },
+                "telecom": [
+                  {
+                    "system": "phone",
+                    "value": "344-845-5689",
+                    "use": "mobile"
+                  }
+                ],
+                "address": [
+                  {
+                    "type": "physical",
+                    "line": [
+                      "321 South Maple Street"
+                    ],
+                    "city": "Scranton",
+                    "state": "PA",
+                    "postalCode": "18503",
+                    "use": "home"
+                  }
+                ],
+                "gender": "male",
+                "birthDate": "1956-12-01"
               }
-            ]
-          } 
-         
+            },
+            {
+              "name": "count",
+              "valueInteger": "3"
+            },
+            {
+              "name": "onlyCertainMatches",
+              "valueBoolean": "false"
+            }
+          ]
+        }
+        
           #puts("request url=" +  url)
-          #puts("request body=" +  body)
+          #puts("request body= #{body}" )
           fhir_operation("Patient/$match", body: body, client: :default, name: nil, headers: { 'Content-Type': 'application/fhir+json' })
 
-          puts("response body=" +  response[:body])
+          #puts("response body=" +  response[:body])
           #fhir_operation("Patient/$match", body: body, client: :default, name: nil, headers: { 'Content-Type': 'application/fhir+json' })
 
           
           #response_json=response[:body]
           output response_json: response[:body]
+          #output response_json: response_json
           assert_response_status(200)
-          assert_resource_type(:bundle)
+          #assert_resource_type(:bundle)
              
       end
     end
     test do
+      input :expectedResultCnt
       input :response_json
+      output :numberOfRecordsReturned
       title 'Patient match - determines whether or not the $match function returns every valid record'
       description %(Match output SHOULD contain every record of every candidate identity, subject to volume limits
       )
       run do
-        puts response_json
-        response=JSON.parse(response_json)
-        puts("Entry Count=" +   response[:total])
+        
+          #puts response_json  
+          response = JSON[response_json]
+          assert_valid_json(response_json, message = "Invalid JSON response received - expected: #{expectedResultCnt} - received: #{numberOfRecordsReturned}")
+          numberOfRecordsReturned = response['total'] 
+          puts "number of records returned in bundle ---- #{numberOfRecordsReturned} " 
+          puts "number of records expected in bundle ---- #{expectedResultCnt} " 
+          assert numberOfRecordsReturned.to_s() == expectedResultCnt.to_s(), "Incorrect Number of Records returned"
+          
+          output numberOfRecordsReturned: numberOfRecordsReturned
+            
+         
       end
+      run do
+        
+        #puts response_json  
+        response = JSON[response_json]
+        assert_valid_json(response_json, message = "Invalid JSON response received - expected: #{expectedResultCnt} - received: #{numberOfRecordsReturned}")
+        numberOfRecordsReturned = response['total'] 
+        puts "number of records returned in bundle ---- #{numberOfRecordsReturned} " 
+        puts "number of records expected in bundle ---- #{expectedResultCnt} " 
+        assert numberOfRecordsReturned.to_s() == expectedResultCnt.to_s(), "Incorrect Number of Records returned"
+        
+        output numberOfRecordsReturned: numberOfRecordsReturned
+          
+       
+    end
     end
   end
 end
